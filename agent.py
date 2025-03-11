e65f77f6a6b2778cd9e8684c85a31deeb51a3500
VuzLe <93986576+Vu0401@users.noreply.github.com>
2025-03-11 22:46:25 +0700

    Update agent.py

--- agent.py
+++ agent.py
@@ -12,19 +12,19 @@ def youtube_summarize(text):
     summerizer = Agent(
         name="Summarizer",
         model="gemini/gemini-2.0-flash-thinking-exp-01-21",
-        instructions = """Summarize the following passage by structuring it into clearly defined sections. The summary **must** follow a two-level structure:  
-        1. Each section **must** start with a **main heading**, placed on a separate line.  
-        2. Directly below each **main heading**, the key ideas **must** be listed as bullet points (e.g., "- key idea").  
+        instructions = """Summarize the following passage **with an objective tone** by structuring it into clearly defined sections. The summary **must** follow a two-level structure:  
+1. Each section **must** start with a **main heading**, placed on a separate line.  
+2. Directly below each **main heading**, the key ideas **must** be listed as bullet points (e.g., "- key idea").  
 
-        Ensure the summary flows logically from a broad overview (macro) to detailed insights (micro) for better readability.  
+Ensure the summary flows logically from a broad overview (macro) to detailed insights (micro) for better readability.  
 
-        ### Rules:  
-        - **Preserve** the original language of the input text.  
-        - **Must** return only the summarized passage
-        - **do not** include introductory sentences or extra commentary.  
-        - **Do not** add, modify, or infer any information not present in the original text.  
-        - **Must** maintain an objective tone throughout.  
-        """,
+### Rules:  
+- **Preserve** the original language of the input text.  
+- **Must** return only the summarized passage.  
+- **Do not** include introductory sentences or extra commentary.  
+- **Do not** add, modify, or infer any information not present in the original text.  
+- **Must** maintain an objective tone throughout.  
+""",
         functions=[],
         model_config={
                 "temperature": 0,
