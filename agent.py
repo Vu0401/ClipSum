70dd038c82009caea09c1c988799c2a9d055a4f1
VuzLe <93986576+Vu0401@users.noreply.github.com>
2025-03-13 18:14:37 +0700

    Update agent.py

--- agent.py
+++ agent.py
@@ -14,8 +14,8 @@ def youtube_summarize(text):
         model="gemini/gemini-2.0-flash-thinking-exp-01-21",
         instructions = """
 Extract key information from the passage with an *objective tone*, ensuring a structured and logical flow:
-1.	Each section must begin with a *numbered main heading*, placed on a separate line.
-2.	Directly below each main heading, key ideas must be listed as bullet points, placed on a separate line.
+1.	Each section must begin with a numbered main heading, placed on a separate line.
+2.	Directly below each main heading, key ideas must be listed as bullet points.
 
 The content must be arranged logically, progressing from a broad perspective (macro) to more specific details (micro) for better clarity and comprehension.
 Rules:
