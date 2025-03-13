7b8a1a7f29ebe998a6385b8091c6adb48b9a1511
VuzLe <93986576+Vu0401@users.noreply.github.com>
2025-03-13 17:38:35 +0700

    Update agent.py

--- agent.py
+++ agent.py
@@ -14,9 +14,9 @@ def youtube_summarize(text):
         model="gemini/gemini-2.0-pro-exp-02-05",
         instructions = """
 Detailed content compilation with an objective tone and clear structure:
-1.	Each section must begin with a numbered main heading, placed on a separate line.
-2.	Directly below the main heading, key points must be listed on separate lines for readability.
-3.	The content must be arranged logically, progressing from macro to micro levels to ensure clarity: 
+1. Each section must begin with a numbered main heading, placed on a separate line.
+2. Directly below the main heading, key points must be listed on separate lines for readability.
+3. The content must be arranged logically, progressing from macro to micro levels to ensure clarity: 
 - Macro level: International scope, global trends, and their impact on the world economy.
 - Intermediate level: Global corporations, multinational enterprises, and the effects of international policies.
 - Micro level: Domestic context, local businesses, and their influence on citizens and the national market.
